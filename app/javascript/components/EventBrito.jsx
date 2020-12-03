08ebb795bdda1a116ddffdb29e02ee0b07a95c17
nonusae <nonusae@gmail.com>
2020-12-03 15:39:11 +0700

    Lift the state into parent component

--- app/javascript/components/EventBrito.jsx
+++ app/javascript/components/EventBrito.jsx
@@ -1,24 +1,62 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import EventForm from './EventForm'
+import axios from 'axios'
 
+import EventForm from './EventForm'
 import EventList from './EventsList'
 
 const EventBrito = props => {
   const [events, setEvents] = React.useState(props.events)
+  const [event, setEvent] = React.useState({
+    title: '',
+    start_datetime: '',
+    location: ''
+  })
 
-  const addNewEvent = (event) => {
-    const newEvents = [event, ...events].sort((a, b) =>
+  const addNewEvent = (data) => {
+    const newEvents = [data, ...events].sort((a, b) =>
       new Date(a.start_datetime) - new Date(b.start_datetime)
     );
 
     setEvents(newEvents)
   }
 
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const newEvent = { ...event }
+    newEvent[name] = e.target.value
+    setEvent(newEvent)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    axios({
+      method: 'POST',
+      url: '/events',
+      data: { event: event },
+      headers: {
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+      }
+    })
+    .then(response => {
+      addNewEvent(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   return (
   <div>
-    <EventForm handleNewEvent={addNewEvent}/>
-    <EventList events={events}  />
+    <EventForm
+      title={event.title}
+      start_datetime={event.start_datetime}
+      location={event.location}
+      handleInput={handleInputChange}
+      handleSubmit={handleSubmit}/>
+    <EventList events={events} />
   </div>
   )
 }
--- app/javascript/components/EventForm.jsx
+++ app/javascript/components/EventForm.jsx
@@ -1,74 +1,33 @@
 import React from 'react'
-import axios from 'axios'
 
-class EventForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      start_datetime: '',
-      location: '',
-    }
-  }
-
-  handleInput = (e) => {
-    const name = e.target.name;
-    const newState = {};
-    newState[name] = e.target.value;
-    this.setState(newState);
-    e.preventDefault();
-  }
-
-  handleSubmit = (e) => {
-    axios({
-      method: 'POST',
-      url: '/events',
-      data: { event: this.state},
-      headers: {
-        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
-      }
-    })
-    .then(response => {
-      this.props.handleNewEvent(response.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-    e.preventDefault()
-  }
-
-  render () {
-    return (
-      <div>
-        <h4>Create an Event:</h4>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={this.state.title}
-            onChange={this.handleInput}
-          />
-          <input
-            type="text"
-            name="start_datetime"
-            placeholder="Date"
-            value={this.state.start_datetime}
-            onChange={this.handleInput}
-          />
-          <input
-            type="text"
-            name="location"
-            placeholder="Location"
-            value={this.state.location}
-            onChange={this.handleInput}
-          />
-          <button type="submit">Create Event</button>
-        </form>
-      </div>
-    )
-  }
-}
+const EventForm = (props) => (
+  <div>
+    <h4>Create an Event:</h4>
+    <form onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={props.title}
+        onChange={props.handleInput}
+      />
+      <input
+        type="text"
+        name="start_datetime"
+        placeholder="Date"
+        value={props.start_datetime}
+        onChange={props.handleInput}
+      />
+      <input
+        type="text"
+        name="location"
+        placeholder="Location"
+        value={props.location}
+        onChange={props.handleInput}
+      />
+      <button type="submit">Create Event</button>
+    </form>
+  </div>
+)
 
 export default EventForm
